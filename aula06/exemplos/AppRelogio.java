bb52ee7aa0d1caaaf41e0b0f11c7d0330a72a22a
BrunoSlemer <21445418@anhembimorumbi.edu.br>
2021-05-23 19:03:30 -0300

    aula06 init

---
+++ aula06/exemplos/AppRelogio.java
@@ -0,0 +1,15 @@
+package exemplos;
+
+public class AppRelogio {
+
+    public static void main(String[] args){
+        Relogio relogio = new Relogio();
+        
+        relogio.hora = 21;
+        relogio.minuto = 4;
+        relogio.segundo = 1;
+
+        relogio.exibir();
+    }
+    
+}
---
+++ aula06/exemplos/Relogio.java
@@ -0,0 +1,9 @@
+package exemplos;
+
+public class Relogio {
+    int hora, minuto, segundo;
+
+    void exibir() {
+        System.out.printf("%02d:%02d:%02d\n", hora, minuto, segundo);
+    }
+}
