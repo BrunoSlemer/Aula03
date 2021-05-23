20fe51a20ea10db6695353d1f562f793ae4dd6da
BrunoSlemer <21445418@anhembimorumbi.edu.br>
2021-05-23 19:52:04 -0300

    inicio da modificaçao

--- aula06/exemplos/AppRelogio.java
+++ aula06/exemplos/AppRelogio.java
@@ -3,13 +3,14 @@ package exemplos;
 public class AppRelogio {
 
     public static void main(String[] args){
-        Relogio relogio = new Relogio();
+        Relogio relogio = new Relogio(2, 23, 45);
         
-        relogio.hora = 21;
-        relogio.minuto = 4;
-        relogio.segundo = 1;
+       /* relogio.hora = -21;
+        relogio.minuto = 400;
+        relogio.segundo = 1;*/
+
 
         relogio.exibir();
     }
     
-}
+} 
--- aula06/exemplos/Relogio.java
+++ aula06/exemplos/Relogio.java
@@ -1,9 +1,15 @@
 package exemplos;
 
 public class Relogio {
-    int hora, minuto, segundo;
+    private int hora, minuto, segundo;
 
-    void exibir() {
+    public Relogio(int hora, int minuto, int segundo){
+        this.hora = hora;
+        this.minuto = minuto;
+        this.segundo = segundo;
+    }
+
+    public void exibir() {
         System.out.printf("%02d:%02d:%02d\n", hora, minuto, segundo);
     }
 }
